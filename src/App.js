3f01282bccd18c67617da72de2f2e6649e77dd84
tparnell <tparnell@cargurus.com>
2019-10-25 13:03:31 -0400

    redux should work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Boxes from './boxes/Boxes'
-import styles from './App.module.css'
+import React from "react";
+import { Provider } from "react-redux";
+import Boxes from "./boxes/Boxes";
+import styles from "./App.module.css";
+import setupStore from "./store/setupStore.js";
 
 function App() {
   return (
-    <div className={styles.grid}>
-      <Boxes sectionName={'What Went Well'} />
-      <Boxes sectionName={'What Could Be Better'} />
-      <Boxes sectionName={'Questions'} />
-    </div>
+    <Provider store={setupStore()}>
+      <div className={styles.grid}>
+        <Boxes sectionName={"What Went Well"} />
+        <Boxes sectionName={"What Could Be Better"} />
+        <Boxes sectionName={"Questions"} />
+      </div>
+    </Provider>
   );
 }
 
