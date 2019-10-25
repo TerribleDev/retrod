406a89db39b86a48f97f2ba143395513eea29d44
Harmony <hkunzler@cargurus.com>
2019-10-25 13:07:41 -0400

    handle when no one has redux devtools

--- src/store/setupStore.js
+++ src/store/setupStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/main.js";
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export default function setupStore() {
   return createStore(
     rootReducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        (devTools || function(f) { return f; })
     )
   );
 }
